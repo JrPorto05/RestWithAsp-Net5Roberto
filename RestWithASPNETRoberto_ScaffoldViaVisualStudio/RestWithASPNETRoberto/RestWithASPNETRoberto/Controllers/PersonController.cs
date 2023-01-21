16c0f00ecd9f3c753e198b3716f9535e24c4a690
JrPorto05 <porto.robertojunior@gmail.com>
2023-01-21 16:45:59 -0300

    Implementação dos verbos

--- RestWithASPNETRoberto_ScaffoldViaVisualStudio/RestWithASPNETRoberto/RestWithASPNETRoberto/Controllers/PersonController.cs
+++ RestWithASPNETRoberto_ScaffoldViaVisualStudio/RestWithASPNETRoberto/RestWithASPNETRoberto/Controllers/PersonController.cs
@@ -1,33 +1,65 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using RestWithASPNETRoberto.Model;
+using RestWithASPNETRoberto.Services;
 
 namespace RestWithASPNETRoberto.Controllers
 {
     [ApiController]
     [Route("[controller]")]
-    public class CalculatorController : ControllerBase
+    public class PersonController : ControllerBase
     {
         
-        private readonly ILogger<CalculatorController> _logger;
+        private readonly ILogger<PersonController> _logger;
+        private IPersonService _personService;
 
-        public CalculatorController(ILogger<CalculatorController> logger)
+        public PersonController(ILogger<PersonController> logger, IPersonService personService)
         {
             _logger = logger;
+
+            _personService = personService;
+
         }
 
-        [HttpGet("sum/{firstNumber}/{secondNumber}")]
-        public IActionResult Sum(string firstNumber, string secondNumber)
+        [HttpGet]
+        public IActionResult Get()
+        {           
+            return Ok(_personService.FindAll());
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult Get(long id)
         {
-           
-            return BadRequest("Invalid Input");
+            var person = _personService.FindByID(id);
+            if (person == null) return NotFound();
+
+            return Ok(person);
         }
 
-     
+        [HttpPost]
+        public IActionResult Post([FromBody] Person person)
+        {
+         
+            if (person == null) return BadRequest();
+
+            return Ok(_personService.Create(person));
+        }
+
+        [HttpPut]
+        public IActionResult Put([FromBody] Person person)
+        {
+
+            if (person == null) return BadRequest();
+
+            return Ok(_personService.Update(person));
+        }
 
 
+        [HttpDelete("{id}")]
+        public IActionResult Delete(long id)
+        {
+            _personService.Delete(id);            
+            return NoContent();
+        }
     }
 }
--- RestWithASPNETRoberto_ScaffoldViaVisualStudio/RestWithASPNETRoberto/RestWithASPNETRoberto/Startup.cs
+++ RestWithASPNETRoberto_ScaffoldViaVisualStudio/RestWithASPNETRoberto/RestWithASPNETRoberto/Startup.cs
@@ -6,6 +6,8 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using RestWithASPNETRoberto.Services;
+using RestWithASPNETRoberto.Services.Implementations;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -26,6 +28,9 @@ namespace RestWithASPNETRoberto
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+
+            //Dependency Injection
+            services.AddScoped<IPersonService, PersonServiceImplementation>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
